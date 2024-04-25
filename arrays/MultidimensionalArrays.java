f5e56ef9b1593eb86d4669033c9f0c4388e1effc
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2024-04-25 16:17:55 +0530

    Added enhanced for loop code for multidimensional arrays

--- arrays/MultidimensionalArrays.java
+++ arrays/MultidimensionalArrays.java
@@ -1,9 +1,6 @@
 package arrays;
 
-import java.util.Arrays;
-
-public class MultidimensionalArrays {
-
+public class MultiDimensionalArrays {
     public static void main(String[] args) {
 
         int[][] arr = {
@@ -16,6 +13,14 @@ public class MultidimensionalArrays {
         for (int row = 0; row < arr.length; row++) {
             for (int col = 0; col < arr[row].length; col++) {
                 System.out.print(arr[row][col] + " ");
+            }
+            System.out.println();
+        }
+
+        // printing using the enhaced for loop
+        for (int[] a : arr) {
+            for (int j : a) {
+                System.out.print(j + " ");
             }
             System.out.println();
         }
