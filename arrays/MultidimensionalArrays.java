102e0dd28244edfddbf5d841d7a840479587acc0
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2024-04-25 17:02:46 +0530

    Taking input in 2D arrays

--- arrays/MultidimensionalArrays.java
+++ arrays/MultidimensionalArrays.java
@@ -1,13 +1,31 @@
 package arrays;
 
+import java.util.Scanner;
+
 public class MultiDimensionalArrays {
     public static void main(String[] args) {
 
-        int[][] arr = {
-                { 1, 2, 3 },
-                { 4, 5 },
-                { 6, 7, 8, 9 }
-        };
+        int[][] arr = new int[3][];
+        Scanner in = new Scanner(System.in);
+
+        // taking the input into the 2D array from the user :-
+        for (int row = 0; row < arr.length; row++) {
+            System.out.println("Enter the number of elements for row number: " + (row + 1));
+            int length = in.nextInt();
+            in.nextLine(); // to consume extra inputs
+            arr[row] = new int[length];
+            System.out.println("Please input the elements for the row number: " + (row + 1));
+            for (int col = 0; col < arr[row].length; col++) {
+                arr[row][col] = in.nextInt();
+            }
+            in.nextLine(); // to consume extra inputs
+        }
+
+        // for (int row = 0; row < arr.length; row++) {
+
+        // }
+
+        in.close();
 
         // printing the 2D array
         for (int row = 0; row < arr.length; row++) {
@@ -18,12 +36,12 @@ public class MultiDimensionalArrays {
         }
 
         // printing using the enhaced for loop
-        for (int[] a : arr) {
-            for (int j : a) {
-                System.out.print(j + " ");
-            }
-            System.out.println();
-        }
+        // for (int[] a : arr) {
+        // for (int j : a) {
+        // System.out.print(j + " ");
+        // }
+        // System.out.println();
+        // }
 
     }
 }
