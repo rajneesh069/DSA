0319e965d907f0b6074508e5f5a5afe5b818d8d2
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2024-06-08 19:17:52 +0530

    find pivot for non-unique elementts

--- javaPlayground/j1.java
+++ javaPlayground/j1.java
@@ -2,11 +2,11 @@ package javaPlayground;
 
 public class j1 {
     public static void main(String[] args) {
-        int[] arr = { 1, 3 };
-        System.out.println(pivotElementIndex(arr));
+        int[] arr = { 2, 2, 1, 2, 2, 2, 2 };
+        System.out.println(pivotElementIndexAmongNonUniqueElements(arr));
     }
 
-    static int pivotElementIndex(int[] arr) {
+    static int pivotElementIndexAmongUniqueElements(int[] arr) {
         int ans = -1;
         int start = 0;
         int end = arr.length - 1;
@@ -25,9 +25,49 @@ public class j1 {
             } else if (mid > 0 && arr[mid - 1] > arr[mid]) {
                 ans = mid - 1;
                 break;
-            } else if (arr[start] <= arr[mid]) {
+            } else if (arr[start] < arr[mid]) {
                 start = mid + 1;
             } else {
+                end = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    static int pivotElementIndexAmongNonUniqueElements(int[] arr) {
+        int ans = -1;
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (mid < arr.length - 1 && arr[mid] > arr[mid + 1]) {
+                /*
+                 * always put the condition first as it would be
+                 * evaluated first hence arr[someIllegalIndex] case
+                 * will never happen as the first condition will be
+                 * false and due to '&&' operator language will never
+                 * evaluate the second expression
+                 */
+                ans = mid;
+                break;
+            } else if (mid > 0 && arr[mid - 1] > arr[mid]) {
+                ans = mid - 1;
+                break;
+            } else if (arr[start] < arr[mid]) { // I'm on the left
+
+                start = mid + 1;
+            } else if (arr[start] == arr[mid] && arr[mid] == arr[end]) {
+                // Ignore the repeating elements
+                if (arr[start] > arr[start + 1]) {
+                    ans = start;
+                    break;
+                }
+                start++;
+                end--;
+            } else if (arr[start] == arr[mid] && arr[mid] != arr[end]) { // I'm on the left
+                start = mid + 1;
+            } else {
+
                 end = mid - 1;
             }
         }
--- leetcode/SearchInRotatedArray.java
+++ leetcode/SearchInRotatedArray.java
@@ -2,13 +2,13 @@ package leetcode;
 
 public class SearchInRotatedArray {
     public static void main(String[] args) {
-        int[] arr = { 1, 1, 3 };
+        int[] arr = { 1, 0, 1, 1, 1 };
         System.out.println(pivotElementWithDuplicateElementsIndex(arr));
         // System.out.println(pivotElementansWithUniqueElementsans(arr));
 
     }
 
-    static boolean searchans(int[] arr, int target) {
+    static boolean searchAns(int[] arr, int target) {
         int ans = -1;
         int ind = pivotElementWithDuplicateElementsIndex(arr);
         int search1 = binarySearchAscending(0, ind, target, arr);
@@ -23,7 +23,7 @@ public class SearchInRotatedArray {
 
     // this won't work for duplicate values
     static int pivotElementansWithUniqueElementsIndex(int[] arr) {
-        int ans = -1;
+        int ans = 0;
         int start = 0;
         int end = arr.length - 1;
         while (start <= end) {
@@ -53,40 +53,59 @@ public class SearchInRotatedArray {
 
     // works for duplicate values
     static int pivotElementWithDuplicateElementsIndex(int[] arr) {
+        /*
+         * int ans = -1;
+         * int start = 0;
+         * int end = arr.length - 1;
+         * while (start <= end) {
+         * int mid = start + (end - start) / 2;
+         * if (mid < arr.length - 1) {
+         * if (arr[mid] > arr[mid + 1]) {
+         * ans = mid;
+         * return ans;
+         * } else if (mid == 0) {
+         * start++;
+         * } else if (arr[mid] < arr[mid - 1]) {
+         * return mid - 1;
+         * } else if (arr[start] > arr[mid]) {
+         * end = mid - 1;
+         * } else if (arr[start] < arr[mid]) {
+         * start = mid + 1;
+         * } else {
+         * if (arr[start] > arr[start + 1]) {
+         * return start;
+         * }
+         * start++;
+         * }
+         * } else {
+         * ans = mid;
+         * break;
+         * }
+         * }
+         * return ans;
+         */
         int ans = -1;
         int start = 0;
         int end = arr.length - 1;
         while (start <= end) {
             int mid = start + (end - start) / 2;
-            if (mid < arr.length - 1) {
-                if (arr[mid] > arr[mid + 1]) {
-                    ans = mid;
-                    return ans;
-                } else if (mid == 0) {
-                    start++;
-                } else if (arr[mid] < arr[mid - 1]) {
-                    return mid - 1;
-                } else if (arr[start] > arr[mid]) {
-                    end = mid - 1;
-                } else if (arr[start] < arr[mid]) {
-                    start = mid + 1;
-                } else {
-                    if (arr[start] > arr[start + 1]) {
-                        return start;
-                    }
-                    start++;
-                }
+            if (mid < arr.length - 1 && arr[mid] > arr[mid + 1]) {
                 /*
-                 * else if(arr[start]<arr[mid] || (arr[start] == arr [mid] && arr[mid] >
-                 * arr[end])){
-                 * start = mid+1;
-                 * }else{
-                 * end = mid-1;
-                 * }
+                 * always put the condition first as it would be
+                 * evaluated first hence arr[someIllegalIndex] case
+                 * will never happen as the first condition will be
+                 * false and due to '&&' operator language will never
+                 * evaluate the second expression
                  */
-            } else {
                 ans = mid;
                 break;
+            } else if (mid > 0 && arr[mid - 1] > arr[mid]) {
+                ans = mid - 1;
+                break;
+            } else if (arr[start] <= arr[mid]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
             }
         }
         return ans;
