ec6a847a03da490d2174f0ebdd365e47be59bfb3
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2024-06-13 14:49:07 +0530

    LCM of 2 numbers

--- bitwiseManipulation/FindLCMOfTwoNumbers.java
+++ bitwiseManipulation/FindLCMOfTwoNumbers.java
@@ -1,6 +1,6 @@
 package bitwiseManipulation;
 
-public class FindLCMOfANumber {
+public class FindLCMOfTwoNumbers {
     public static void main(String[] args) {
         System.out.println(LCM(33, 11));
     }
