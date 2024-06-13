5388678799c0c54d9fe65ea5104414e9e3671474
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2024-06-13 15:33:25 +0530

    find a to the power of b, in O(log(b)) time complexity

--- bitwiseManipulation/FindAToThePowerOfB.java
+++ bitwiseManipulation/FindAToThePowerOfB.java
@@ -2,10 +2,20 @@ package bitwiseManipulation;
 
 public class FindAToThePowerOfB {
     public static void main(String[] args) {
-
+        System.out.println(pow(2, 4));
     }
 
-    public static int powerOfAToB(int a, int b) {
-        
+    // O(log(b)) -> time complexity
+
+    public static int pow(int base, int index) {
+        int ans = 1;
+        while (index != 0) {
+            if ((index & 1) != 0) {
+                ans = ans * base;
+            }
+            base *= base;
+            index = index >> 1;
+        }
+        return ans;
     }
 }
