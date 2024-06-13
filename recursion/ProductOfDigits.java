7facb9edb004ca847a7e22f85c83767ab513d311
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2024-06-13 18:51:49 +0530

    product of digits

---
+++ recursion/ProductOfDigits.java
@@ -0,0 +1,16 @@
+package recursion;
+
+public class ProductOfDigits {
+    public static void main(String[] args) {
+        System.out.println(product(12356));
+    }
+
+    static int product(int num) {
+        if (num == 0) {
+            return 1;
+        }
+        int digit = num % 10;
+        num /= 10;
+        return digit * product(num);
+    }
+}
