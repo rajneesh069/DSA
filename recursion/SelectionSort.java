206994e4e804a02192a442e7f933f797ef559f46
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2024-06-14 22:39:14 +0530

    selection sort using recursion another way

--- recursion/SelectionSort.java
+++ recursion/SelectionSort.java
@@ -5,8 +5,7 @@ import java.util.Arrays;
 public class SelectionSort {
     public static void main(String[] args) {
         int[] arr = { 1, 3, 2, 4 };
-        System.out.println("maxIndex: " + maxIndex(arr, arr.length - 1, arr.length - 1));
-        selection(arr, arr.length - 1);
+        selection(arr, arr.length, 0, 0);
         System.out.println(Arrays.toString(arr));
     }
 
@@ -16,23 +15,39 @@ public class SelectionSort {
         arr[first] = temp;
     }
 
-    static int maxIndex(int[] arr, int end, int max) {
-        if (end < 0) {
-            return max;
-        } else if (arr[end] > arr[max]) {
-            max = end;
+    static void selection(int[] arr, int r, int c, int max) { // think in 2d
+        if (r == 0) {
+            return;
+        } else if (r > c) {
+            if (arr[c] > arr[max]) {
+                max=c;
+            }
+            selection(arr, r, c + 1, max);
+        } else {
+            swap(arr, max, r-1);
+            selection(arr, --r, 0, 0);
         }
-        return maxIndex(arr, --end, max);
+
     }
 
-    static void selection(int[] arr, int end) {
-        if (end == 0) {
-            return;
-        }
-        swap(arr, end, maxIndex(arr, end, end)); // important
-        selection(arr, --end);
-        // {1,3,2,4}
-        // {1,2,3,4}
-        //
-    }
+    // static int maxIndex(int[] arr, int end, int max) {
+    // if (end < 0) {
+    // return max;
+    // } else if (arr[end] > arr[max]) {
+    // max = end;
+    // }
+    // return maxIndex(arr, --end, max);
+    // }
+
+    // static void selection(int[] arr, int end) {
+    // if (end == 0) {
+    // return;
+    // }
+    // swap(arr, end, maxIndex(arr, end, end)); // important
+    // selection(arr, --end);
+    // // {1,3,2,4}
+    // // {1,2,3,4}
+    // //
+    // }
+
 }
