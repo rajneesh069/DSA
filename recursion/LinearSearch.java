43755a2ba494a48c0e0f8da02dff51a5515be703
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2024-06-14 11:36:19 +0530

    find all indices of the target in an array using recursion and linear search

--- recursion/LinearSearch.java
+++ recursion/LinearSearch.java
@@ -1,10 +1,13 @@
 package recursion;
 
+import java.util.ArrayList;
+
 public class LinearSearch {
     public static void main(String[] args) {
         int[] arr = { 1, 2, 78, 32, 9 };
         int target = 69;
-        System.out.println(search(arr, target, 0));
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        System.out.println(findAllIndices(arr, target, 0, list));
     }
 
     static int search(int[] arr, int target, int start) {
@@ -18,5 +21,22 @@ public class LinearSearch {
             return start;
         }
         return search(arr, target, ++start);
+    }
+
+    static boolean doesExistInArray(int[] arr, int target, int start) {
+        if (start == arr.length) {
+            return false;
+        }
+        return arr[start] == target || doesExistInArray(arr, target, ++start);
+    }
+
+    static ArrayList<Integer> findAllIndices(int[] arr, int target, int start, ArrayList<Integer> list) {
+        if (start == arr.length) {
+            return list;
+        }
+        if (arr[start] == target) {
+            list.add(start);
+        }
+        return findAllIndices(arr, target, ++start, list);
     }
 }
