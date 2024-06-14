08748ab09366207a0a247f5780509e5f53ca68bd
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2024-06-14 14:28:21 +0530

    apply the not taking extra argument thing to counting zeroes in a number and it works!

--- recursion/CountZeroes.java
+++ recursion/CountZeroes.java
@@ -2,10 +2,22 @@ package recursion;
 
 public class CountZeroes {
     public static void main(String[] args) {
-        System.out.println(countZeroes(1000, 0));
+        System.out.println(countZeroes(100));
     }
 
-    static int countZeroes(int num, int count) {
+    // static int countZeroes(int num, int count) {
+    // int digit = num % 10;
+    // if (num == 0) {
+    // return count;
+    // }
+    // if (digit == 0) {
+    // count++;
+    // }
+    // num = num / 10;
+    // return countZeroes(num, count);
+    // }
+    static int countZeroes(int num) {
+        int count = 0;
         int digit = num % 10;
         if (num == 0) {
             return count;
@@ -14,6 +26,10 @@ public class CountZeroes {
             count++;
         }
         num = num / 10;
-        return countZeroes(num, count);
+        int prevCount = countZeroes(num); // the prodigal line returns, although not recommended to use,
+        // as count variable is declared everytime
+        System.out.println("count: " + count);
+        count += prevCount;
+        return count;
     }
 }
