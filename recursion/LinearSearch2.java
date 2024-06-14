9dca1756dbaf64975a6c3baae1387016d2e807c7
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2024-06-14 14:18:12 +0530

    linear search without passing the array list as an argument

---
+++ recursion/LinearSearch2.java
@@ -0,0 +1,28 @@
+package recursion;
+
+import java.util.ArrayList;
+
+public class LinearSearch2 {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 69, 3 };
+        int target = 69;
+        int start = 0;
+        System.out.println(search(arr, target, start));
+
+    }
+
+    static ArrayList<Integer> search(int[] arr, int target, int start) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        if (start == arr.length) {
+            return list;
+        }
+        if (arr[start] == target) {
+            list.add(start);
+        }
+
+        ArrayList<Integer> ansFromBelow = search(arr, target, ++start); // very clever line!
+        list.addAll(ansFromBelow);
+
+        return list;
+    }
+}
