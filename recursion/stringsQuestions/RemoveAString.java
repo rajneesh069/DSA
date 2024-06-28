3dc1c057cd11ee86a6ac0dbfd5bd03b46abe7c22
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2024-06-28 20:04:09 +0530

    remove a string

--- recursion/stringsQuestions/RemoveAString.java
+++ recursion/stringsQuestions/RemoveAString.java
@@ -1,5 +1,19 @@
 package recursion.stringsQuestions;
 
 public class RemoveAString {
-    
+    public static void main(String[] args) {
+        String s = new String("bccappleadseapple");
+        System.out.println(removeString(s));
+    }
+
+    static String removeString(String s) {
+        if (s.isEmpty()) {
+            return "";
+        }
+
+        if (s.startsWith("apple")) {
+            return removeString(s.substring(5));
+        }
+        return s.charAt(0) + removeString(s.substring(1));
+    }
 }
