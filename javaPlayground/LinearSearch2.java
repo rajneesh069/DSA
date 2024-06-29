9a83fe3f938d74d44a8ac161f6c088f63646e29f
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2024-06-29 20:59:48 +0530

    linear search using recursion

---
+++ javaPlayground/LinearSearch2.java
@@ -0,0 +1,24 @@
+package javaPlayground;
+
+import java.util.ArrayList;
+
+public class LinearSearch2 {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 2 };
+        System.out.println(search(arr, 2, 0));
+    }
+
+    static ArrayList<Integer> search(int[] arr, int target, int start) {
+        ArrayList<Integer> ans = new ArrayList<Integer>();
+        if (start == arr.length) {
+            return ans;
+        }
+        if (arr[start] == target) {
+            ans.add(start);
+        }
+
+        ArrayList<Integer> temp = search(arr, target, ++start);
+        ans.addAll(temp);
+        return ans;
+    }
+}
