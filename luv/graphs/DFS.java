bff876d40f4b3b6bb0114e821460fc3cf9527446
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2024-07-24 20:16:27 +0530

    wrote basic DFS, not the correct one, but the basic one

--- luv/graphs/DFS.java
+++ luv/graphs/DFS.java
@@ -2,10 +2,18 @@ package luv.graphs;
 
 public class DFS {
     public static void main(String[] args) {
-        
+
     }
 
-    static void dfs(int vertex){
+    static int n, m;
+    static int[][] graph = new int[n][n];
 
+    static boolean[] vis = new boolean[n];
+
+    static void dfs(int vertex) {
+        // Basic DFS, not correct but basic DFS algo.
+        for (int child : graph[vertex]) {
+            dfs(child);
+        }
     }
 }
